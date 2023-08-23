a2b72fa9da98f778f4b07ffaa76e0e1c919bfe75
rubencho <rudargeneira80@gmail.com>
2023-08-23 16:46:57 -0400

    ejercicio 3 java script fundamentos

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,82 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+
+
+if(x===y){
+   console.log("true");
+   
+}else {
+   console.log("false");
+}
+sonIguales(5,3);
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-}
+if (str1.compareTo(str2)==0)
+  {  return true;
+  }  else 
+     { return false; }
+  }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+
+
+if (num<90){
+   return true;
+}   else {
+      if (num>=90){
+        return false;  
+      } 
+
+}
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+
+     if (num>50)
+     {
+        return true;
+     }  else if(num<=50) {
+          return false;
+         }
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+    r=num%2;
+
+    if(r===0)
+    {  return true;
+
+    } else 
+      {  return false; }
+
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+
+   r=num%2;
+
+   if(r!=0)
+   {  return true;
+
+   } else 
+     {  return false; }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
---
+++ ejercio3.js
@@ -0,0 +1,22 @@
+
+/*function sonIguales(x, y) {
+    // Retorna true si "x" e "y" son iguales.
+    // De lo contrario, retorna false.
+    // Tu código:
+ 
+    if (x===y){
+       console.log("true");
+       
+    }
+ return(x)
+    
+ }
+
+*/
+
+//Función: sumaTres
+var sumaTres (x) {
+    return x + 3;
+  }
+ 
+  sumaTres(5);
