fb8b3682e7c94da3b6b47f70f446e318c726e33c
rubencho <rudargeneira80@gmail.com>
2023-09-19 23:22:01 -0400

    M07

---
+++ prueba ojetos.js
@@ -0,0 +1,14 @@
+Class Auto {
+    constructor (marca,ano,color) {
+        this.marca;
+        this.ano;
+        this.color;
+           
+    
+    }
+      
+  }
+
+var autoNuevo=new auto("fiat","2005","Rojo");
+      System.out.println(autoNuevo);
+      System.out.println(autoNuevo.marca);
