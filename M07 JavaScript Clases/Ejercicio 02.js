c28d531e9859cce4f1d1dc26eb010ab9eedde562
rubencho <rudargeneira80@gmail.com>
2023-09-21 14:23:04 -0400

    M07 1 Y 2

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,18 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+
+    
+    constructor(nombre,apellido,edad,domicilio){
+
+      this.nombre=nombre;
+      this.apellido=apellido;
+      this.edad=edad;
+      this.domicilio;
+    }
+    datos=function(){
+    console.log(this.nombre+this.apellido+this.edad+this.domicilio);
+    }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +25,9 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+
+   var datos=new Persona("Pedro","Perez",30,"Italia");
+   console.log(datos.Persona);
 }
 
 function agregarMetodo() {
@@ -20,8 +35,16 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   class Persona{
+      constructor(nombre,edad){
+         this.nombre=nombre;
+         this.edad=edad;
+      }
+      muestra=function(nombre,eddad){
+         console.log(this.nombre + this.edad);
+      }
+   }
 }
-
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    crearInstanciaPersona,
